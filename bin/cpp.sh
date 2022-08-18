60dbdfa29bdc0914c92fd9b80b4051de6dba3edc
iconicisgod@gmail.com <iconicisgod@gmail.com>
2022-08-18 19:22:44 +0530

    n

--- bin/cpp.sh
+++ bin/cpp.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env node
 
 const path = require('path');
-require(path.resolve('./index.js')).createHTML;
+require(path.resolve('./index.js')).createcpp;
--- index.js
+++ index.js
@@ -1,12 +1,23 @@
+
+//external modules
+
 const fs = require('fs');
 const path = require('path');
 
+//export ing functions
 exports.createHTML = createHTML;
 exports.createjs = createjs;
+exports.createjava = createjava;
+exports.createpy = createpy;
+exports.createcpp = createcpp;
+
+//location
 const location = process.argv[2];
 
-function createHTML() {
-    let html = `
+//File Creation
+
+//code example created
+let html = `
 <!DOCTYPE html>
 <html>
 
@@ -17,27 +28,17 @@ function createHTML() {
 </head>
 
 <body>
-
+    <p>Believe in yourself</p>
 </body>
 
 </html>
 
     `
-    fs.writeFileSync(`${process.cwd()}/${location}`, html);
-}
-
-function createjs(){
-    let js = `
-    console.log("Hello, fellow!");
-    //Your Code:-
+let js = `
+console.log("Hello, fellow!");
+//Your Code:-
     `
-    fs.writeFileSync(`${process.cwd()}/${location}`, js);
-}
-
-
-
-function createjava(){
-    let java = `
+let java = `
 class Hello {
     public static void main(String[] args) {
         System.out.println("Hello, fellow!"); 
@@ -45,18 +46,60 @@ class Hello {
     }
 }
     `
+let python = `
+print('Hello, fellow!')
+#Your code
+    `
+let cpp = `#include <iostream>
+
+int main() {
+    std::cout << "Hello, Fellow!";
+    return 0;
+}
+    `
+    
+
+
+
+//Hyper text markup language
+function createHTML() {
+    fs.writeFileSync(`${process.cwd()}/${location}`, html);
+}
+
+
+//javascript
+function createjs(){
+    fs.writeFileSync(`${process.cwd()}/${location}`, js);
+}
+
+
+
+//java
+function createjava(){
     fs.writeFileSync(`${process.cwd()}/${location}`, java);
 }
 
+
+
+//python
 function createpy(){
-    let python = `
-    print('Hello, fellow!')
-    #Your code
-    `
     fs.writeFileSync(`${process.cwd()}/${location}`, python);
 }
 
-createHTML()
-createjs()
-createjava()
-createpy()
+
+
+//C++
+function createcpp(){
+    fs.writeFileSync(`${process.cwd()}/${location}`, cpp);
+}
+
+
+
+//web bundle
+function webb(){
+    fs.mkdir(`${process.cwd()}/${location}`);
+}
+
+
+
+webb();
